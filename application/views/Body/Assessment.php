38e5325c5dc3576a29c4b2b3a39ca13c813fa55c
Gerard <gerarddilla@gmail.com>
2021-03-15 11:28:40 +0800

    Added payment plan choices

--- application/views/Body/Assessment.php
+++ application/views/Body/Assessment.php
@@ -4,10 +4,13 @@
         </div>
         <div class="card-body row">
 
+            <!-- STUDENT INFORMATION TAB -->
+            <!-- /STUDENT INFORMATION TAB -->
+
+            <!-- ADVISING TAB -->
             <div class="col-md-12">
                 <div class="row">
-                    <hr>
-                    <h6 class="col-md-12" style="margin-bottom:15px">CHOOSE ONE OF THE FOLLOWING:</h6>
+                    <h6 class="col-md-12" style="margin-bottom:15px">1. COMPLETE THE FOLLOWING:</h6>
                     <div class="col-md-6 mb-4">
                         <div class="input-group mb-3">
                             <label class="input-group-text" for="inputGroupSelect01">School Year</label>
@@ -54,14 +57,15 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-md-12">
                 <hr>
                 <div class="row">
                     <div class="col-md-12">
-                        <h6>SUGGESTED SUBJECTS</h6>
+                        <h6>2. CHOOSE SUBJECTS</h6>
                         <br>
                         <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#subjectModal">Add Subjects</a>
-                        <a href="#" class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="scheduleModal">View Schedule Plot</a>
+                        <a href="#" class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#scheduleModal">View Schedule Plot</a>
                     </div>
                 </div>
                 <br>
@@ -114,9 +118,69 @@
                 </table>
             </div>
 
+            <div class="col-md-12">
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h6>3. SELECT PAYMENT PLAN</h6>
+                        <br>
+
+                        <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off" checked="">
+                        <label class="btn btn-outline-primary" for="success-outlined">
+                            INSTALLMENT
+                        </label>
+
+                        <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="danger-outlined">
+                            FULL PAYMENT
+                        </label>
+
+                    </div>
+
+                    <div class="col-md-6">
+                        <br>
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td>OTHER FEES:</td>
+                                    <td>10000</td>
+                                </tr>
+                                <tr>
+                                    <td>MISC FEES</td>
+                                    <td>10000</td>
+                                </tr>
+                                <tr>
+                                    <td>LAB FEES</td>
+                                    <td>10000</td>
+                                </tr>
+                                <tr>
+                                    <td>TUITION FEES</td>
+                                    <td>10000</td>
+                                </tr>
+                                <tr>
+                                    <td>TOTAL FEES:</td>
+                                    <td>10000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+
+                    </div>
+                </div>
+            </div>
+            <!-- /ADVISING TAB -->
+
+            <!-- PRE REGISTRATION TAB -->
+            <!-- /PRE REGISTRATION TAB -->
+
+            <!-- PAYMENT TAB -->
+            <!-- /PAYMENT TAB -->
+
         </div>
 
-        <!-- Modal Content -->
+        <!-- Modal Contents -->
+
+        <!-- Subject Choices -->
         <div class="modal fade text-left w-100" id="subjectModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel16" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
                 <div class="modal-content">
@@ -173,6 +237,86 @@
                                     <td><a href="#" class="btn btn-primary">Add Subject</a></td>
                                 </tr>
 
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                            <i class="bx bx-x d-block d-sm-none"></i>
+                            <span class="d-none d-sm-block">Close</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Schedule Plotting -->
+        <div class="modal fade text-left w-100" id="scheduleModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel16" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="myModalLabel16">SCHEDULE PLOT</h4>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>MON</th>
+                                    <th>TUE</th>
+                                    <th>WED</th>
+                                    <th>THURS</th>
+                                    <th>FRI</th>
+                                    <th>SAT</th>
+                                    <th>SUN</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>7:00 AM</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                </tr>
+                                <tr>
+                                    <th>7:30 AM</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                </tr>
+                                <tr>
+                                    <th>8:00 AM</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                </tr>
+                                <tr>
+                                    <th>8:30 AM</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                </tr>
                             </tbody>
                         </table>
 

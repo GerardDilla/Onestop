9b6f3ea4ad13a582c6c5247fcbf359e7a5edfabe
Gerard <gerarddilla@gmail.com>
2021-03-15 10:34:22 +0800

    Added modal for subject choices

--- application/views/Body/Assessment.php
+++ application/views/Body/Assessment.php
@@ -2,86 +2,133 @@
     <div class="card">
         <div class="card-header">
         </div>
-        <div class="card-body">
+        <div class="card-body row">
 
-
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link active" id="confirmation-tab" data-bs-toggle="tab" href="#confirmation" role="tab" aria-controls="confirmation" aria-selected="true"><b>STEP 1</b>: CONFIRM INFORMATION</a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="assessment-tab" data-bs-toggle="tab" href="#assessment" role="tab" aria-controls="assessment" aria-selected="false"><b>STEP 2</b>: CHOOSE SUBJECTS</a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="payment-tab" data-bs-toggle="tab" href="#payment" role="tab" aria-controls="payment" aria-selected="false"><b>STEP 3</b>: PAYMENT</a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="registration-tab" data-bs-toggle="tab" href="#registration" role="tab" aria-controls="registration" aria-selected="false"><b>STEP 4</b>: REGISTRATION FORM</a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="citizenship-tab" data-bs-toggle="tab" href="#citizenship" role="tab" aria-controls="citizenship" aria-selected="false"><b>STEP 5</b>: DIGITAL CITIZENSHIP</a>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="confirmation" role="tabpanel" aria-labelledby="home-tab">
-                    c
-                </div>
-
-                <div class="tab-pane fade row" id="assessment" role="tabpanel" style="padding-top:5%;" aria-labelledby="assessment-tab">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <hr>
-                            <h6 class="col-md-12" style="margin-bottom:15px">CHOOSE ONE OF THE FOLLOWING:</h6>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">School Year</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">2020-2021</option>
-                                        <option value="2">2020-2021</option>
-                                        <option value="3">2020-2021</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Semester</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Curriculum</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Section</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <hr>
+                    <h6 class="col-md-12" style="margin-bottom:15px">CHOOSE ONE OF THE FOLLOWING:</h6>
+                    <div class="col-md-6 mb-4">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01">School Year</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                                <option value="1">2020-2021</option>
+                                <option value="2">2020-2021</option>
+                                <option value="3">2020-2021</option>
+                            </select>
                         </div>
                     </div>
+                    <div class="col-md-6 mb-4">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01">Semester</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                                <option value="1">FIRST</option>
+                                <option value="2">SECOND</option>
+                                <option value="3">SUMMER</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01">Curriculum</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                                <option value="1">FIRST</option>
+                                <option value="2">SECOND</option>
+                                <option value="3">SUMMER</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01">Section</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                                <option value="1">FIRST</option>
+                                <option value="2">SECOND</option>
+                                <option value="3">SUMMER</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <hr>
+                <div class="row">
                     <div class="col-md-12">
-                        <hr>
                         <h6>SUGGESTED SUBJECTS</h6>
-                        <table class="table table-striped" id="table1">
+                        <br>
+                        <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#subjectModal">Add Subjects</a>
+                        <a href="#" class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="scheduleModal">View Schedule Plot</a>
+                    </div>
+                </div>
+                <br>
+                <table class="table table-striped" id="queueTable">
+                    <thead>
+                        <tr>
+                            <th>Sched Code</th>
+                            <th>Course Code</th>
+                            <th>Course Title</th>
+                            <th>Section</th>
+                            <th>Units</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>20202021</td>
+                            <td>ITC123</td>
+                            <td>Programming Fundamentals</td>
+                            <td>BSIT1A</td>
+                            <td>5</td>
+                            <td><a href="#" class="btn btn-sm btn-info">Remove</a></td>
+                        </tr>
+                        <tr>
+                            <td>20202022</td>
+                            <td>ITC123</td>
+                            <td>Programming Fundamentals</td>
+                            <td>BSIT1A</td>
+                            <td>5</td>
+                            <td><a href="#" class="btn btn-sm btn-info">Remove</a></td>
+                        </tr>
+                        <tr>
+                            <td>20202023</td>
+                            <td>ITC123</td>
+                            <td>Programming Fundamentals</td>
+                            <td>BSIT1A</td>
+                            <td>5</td>
+                            <td><a href="#" class="btn btn-sm btn-info">Remove</a></td>
+                        </tr>
+                        <tr>
+                            <td>20202023</td>
+                            <td>ITC123</td>
+                            <td>Programming Fundamentals</td>
+                            <td>BSIT1A</td>
+                            <td>5</td>
+                            <td><a href="#" class="btn btn-sm btn-info">Remove</a></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+        <!-- Modal Content -->
+        <div class="modal fade text-left w-100" id="subjectModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel16" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="myModalLabel16">ADD SUBJECTS</h4>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <table class="table table-striped" id="subjectTable">
                             <thead>
                                 <tr>
                                     <th>Sched Code</th>
@@ -99,7 +146,7 @@
                                     <td>Programming Fundamentals</td>
                                     <td>BSIT1A</td>
                                     <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
+                                    <td><a href="#" class="btn btn-primary">Add Subject</a></td>
                                 </tr>
                                 <tr>
                                     <td>20202022</td>
@@ -107,7 +154,7 @@
                                     <td>Programming Fundamentals</td>
                                     <td>BSIT1A</td>
                                     <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
+                                    <td><a href="#" class="btn btn-primary">Add Subject</a></td>
                                 </tr>
                                 <tr>
                                     <td>20202023</td>
@@ -115,7 +162,7 @@
                                     <td>Programming Fundamentals</td>
                                     <td>BSIT1A</td>
                                     <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
+                                    <td><a href="#" class="btn btn-primary">Add Subject</a></td>
                                 </tr>
                                 <tr>
                                     <td>20202023</td>
@@ -123,86 +170,22 @@
                                     <td>Programming Fundamentals</td>
                                     <td>BSIT1A</td>
                                     <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
+                                    <td><a href="#" class="btn btn-primary">Add Subject</a></td>
                                 </tr>
 
                             </tbody>
                         </table>
+
                     </div>
-                </div>
-
-
-                <div class="tab-pane fade" id="payment" role="tabpanel" aria-labelledby="payment-tab">
-                    p
-                </div>
-                <div class="tab-pane fade" id="registration" role="tabpanel" aria-labelledby="registration-tab">
-                    r
-                </div>
-                <div class="tab-pane fade" id="citizenship" role="tabpanel" aria-labelledby="citizenship-tab">
-                    ct
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                            <i class="bx bx-x d-block d-sm-none"></i>
+                            <span class="d-none d-sm-block">Close</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- 
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <p class='my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla ut nulla
-                    neque. Ut hendrerit nulla a euismod pretium.
-                    Fusce venenatis sagittis ex efficitur suscipit. In tempor mattis
-                    fringilla. Sed id
-                    tincidunt orci, et volutpat ligula.
-                    Aliquam sollicitudin sagittis ex, a rhoncus nisl feugiat quis. Lorem
-                    ipsum dolor sit
-                    amet, consectetur adipiscing elit.
-                    Nunc ultricies ligula a tempor vulputate. Suspendisse pretium mollis
-                    ultrices.</p>
-            </div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                Integer interdum diam eleifend metus lacinia, quis gravida eros mollis.
-                Fusce non sapien
-                sit amet magna dapibus
-                ultrices. Morbi tincidunt magna ex, eget faucibus sapien bibendum non. Duis
-                a mauris ex.
-                Ut finibus risus sed massa
-                mattis porta. Aliquam sagittis massa et purus efficitur ultricies. Integer
-                pretium dolor
-                at sapien laoreet ultricies.
-                Fusce congue et lorem id convallis. Nulla volutpat tellus nec molestie
-                finibus. In nec
-                odio tincidunt eros finibus
-                ullamcorper. Ut sodales, dui nec posuere finibus, nisl sem aliquam metus, eu
-                accumsan
-                lacus felis at odio. Sed lacus
-                quam, convallis quis condimentum ut, accumsan congue massa. Pellentesque et
-                quam vel
-                massa pretium ullamcorper vitae eu
-                tortor.
-            </div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                <p class="mt-2">Duis ultrices purus non eros fermentum hendrerit. Aenean
-                    ornare interdum
-                    viverra. Sed ut odio velit. Aenean eu diam
-                    dictum nibh rhoncus mattis quis ac risus. Vivamus eu congue ipsum.
-                    Maecenas id
-                    sollicitudin ex. Cras in ex vestibulum,
-                    posuere orci at, sollicitudin purus. Morbi mollis elementum enim, in
-                    cursus sem
-                    placerat ut.</p>
-            </div>
-        </div> -->
-
 
     </div>
 </section>
@@ -211,6 +194,10 @@
 <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
 <script>
     // Simple Datatable
-    let table1 = document.querySelector('#table1');
-    let dataTable = new simpleDatatables.DataTable(table1);
+    let queueTable = document.querySelector('#queueTable');
+    let dataTable1 = new simpleDatatables.DataTable(queueTable);
+
+    // Simple Datatable
+    let subjectTable = document.querySelector('#subjectTable');
+    let dataTable2 = new simpleDatatables.DataTable(subjectTable);
 </script>

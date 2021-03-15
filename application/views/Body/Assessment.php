4c4de9f1709512beb2e9779c4c4074a50f063629
Gerard <gerarddilla@gmail.com>
2021-03-15 09:31:23 +0800

    Rough Draft of tabs

--- application/views/Body/Assessment.php
+++ application/views/Body/Assessment.php
@@ -3,32 +3,31 @@
         <div class="card-header">
         </div>
         <div class="card-body">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="table-responsive">
-                        <table class="table table-lg ">
-                            <tbody>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">NAME:</td>
-                                    <td>Juan Dela Cruz</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">REFERENCE NUMBER:</td>
-                                    <td>12345</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">COURSE:</td>
-                                    <td>BSIT</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">MAJOR:</td>
-                                    <td>N/A</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+
+
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="confirmation-tab" data-bs-toggle="tab" href="#confirmation" role="tab" aria-controls="confirmation" aria-selected="true"><b>STEP 1</b>: CONFIRM INFORMATION</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="assessment-tab" data-bs-toggle="tab" href="#assessment" role="tab" aria-controls="assessment" aria-selected="false"><b>STEP 2</b>: CHOOSE SUBJECTS</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="payment-tab" data-bs-toggle="tab" href="#payment" role="tab" aria-controls="payment" aria-selected="false"><b>STEP 3</b>: PAYMENT</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="registration-tab" data-bs-toggle="tab" href="#registration" role="tab" aria-controls="registration" aria-selected="false"><b>STEP 4</b>: REGISTRATION FORM</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="citizenship-tab" data-bs-toggle="tab" href="#citizenship" role="tab" aria-controls="citizenship" aria-selected="false"><b>STEP 5</b>: DIGITAL CITIZENSHIP</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="confirmation" role="tabpanel" aria-labelledby="home-tab">
+                    c
                 </div>
-                <div class="col-md-9 row">
+
+                <div class="tab-pane fade row" id="assessment" role="tabpanel" style="padding-top:5%;" aria-labelledby="assessment-tab">
                     <div class="col-md-12">
                         <div class="row">
                             <hr>
@@ -77,7 +76,6 @@
                                     </select>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                     <div class="col-md-12">
@@ -131,12 +129,82 @@
                             </tbody>
                         </table>
                     </div>
-                    <div class="col-md-12">
-                    </div>
+                </div>
+
+
+                <div class="tab-pane fade" id="payment" role="tabpanel" aria-labelledby="payment-tab">
+                    p
+                </div>
+                <div class="tab-pane fade" id="registration" role="tabpanel" aria-labelledby="registration-tab">
+                    r
+                </div>
+                <div class="tab-pane fade" id="citizenship" role="tabpanel" aria-labelledby="citizenship-tab">
+                    ct
                 </div>
             </div>
-
         </div>
+        <!-- 
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <p class='my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nulla ut nulla
+                    neque. Ut hendrerit nulla a euismod pretium.
+                    Fusce venenatis sagittis ex efficitur suscipit. In tempor mattis
+                    fringilla. Sed id
+                    tincidunt orci, et volutpat ligula.
+                    Aliquam sollicitudin sagittis ex, a rhoncus nisl feugiat quis. Lorem
+                    ipsum dolor sit
+                    amet, consectetur adipiscing elit.
+                    Nunc ultricies ligula a tempor vulputate. Suspendisse pretium mollis
+                    ultrices.</p>
+            </div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                Integer interdum diam eleifend metus lacinia, quis gravida eros mollis.
+                Fusce non sapien
+                sit amet magna dapibus
+                ultrices. Morbi tincidunt magna ex, eget faucibus sapien bibendum non. Duis
+                a mauris ex.
+                Ut finibus risus sed massa
+                mattis porta. Aliquam sagittis massa et purus efficitur ultricies. Integer
+                pretium dolor
+                at sapien laoreet ultricies.
+                Fusce congue et lorem id convallis. Nulla volutpat tellus nec molestie
+                finibus. In nec
+                odio tincidunt eros finibus
+                ullamcorper. Ut sodales, dui nec posuere finibus, nisl sem aliquam metus, eu
+                accumsan
+                lacus felis at odio. Sed lacus
+                quam, convallis quis condimentum ut, accumsan congue massa. Pellentesque et
+                quam vel
+                massa pretium ullamcorper vitae eu
+                tortor.
+            </div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                <p class="mt-2">Duis ultrices purus non eros fermentum hendrerit. Aenean
+                    ornare interdum
+                    viverra. Sed ut odio velit. Aenean eu diam
+                    dictum nibh rhoncus mattis quis ac risus. Vivamus eu congue ipsum.
+                    Maecenas id
+                    sollicitudin ex. Cras in ex vestibulum,
+                    posuere orci at, sollicitudin purus. Morbi mollis elementum enim, in
+                    cursus sem
+                    placerat ut.</p>
+            </div>
+        </div> -->
+
+
+    </div>
 </section>
 <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>

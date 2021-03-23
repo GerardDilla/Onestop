39ab4935c961456dd10eb5d55eecf279dfdb0172
Gerard <gerarddilla@gmail.com>
2021-03-23 16:26:49 +0800

    Revert "Revert "Merge branch 'feature-assessment-module' into main""
    This reverts commit fe4eeec71b1abebc5aebcd3fe8315597063a18c4.

--- application/views/Body/Assessment.php
+++ application/views/Body/Assessment.php
@@ -1,148 +1,105 @@
+<!-- <?php $this->load->helper('string');
+        echo random_string('alnum', 20); ?> -->
+<!-- Unused css
+    <link href="<?php echo base_url() ?>assets/js/wizard/bootstrap.min.css" rel="stylesheet" /> -->
+<link href="<?php echo base_url() ?>assets/js/wizard/paper-bootstrap-wizard.css" rel="stylesheet" />
 <section class="section">
+    
     <div class="card">
-        <div class="card-header">
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="table-responsive">
-                        <table class="table table-lg ">
-                            <tbody>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">NAME:</td>
-                                    <td>Juan Dela Cruz</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">REFERENCE NUMBER:</td>
-                                    <td>12345</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">COURSE:</td>
-                                    <td>BSIT</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-bold-500" style="font-weight:bold">MAJOR:</td>
-                                    <td>N/A</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="col-md-9 row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <hr>
-                            <h6 class="col-md-12" style="margin-bottom:15px">CHOOSE ONE OF THE FOLLOWING:</h6>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">School Year</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">2020-2021</option>
-                                        <option value="2">2020-2021</option>
-                                        <option value="3">2020-2021</option>
-                                    </select>
-                                </div>
+        <!-- <div class="card-header">
+        </div> -->
+        <div class="row">
+            <!-- Wizard container -->
+            <div class="wizard-container">
+                <div class="card wizard-card" data-color="red" id="wizardProfile">
+                    <form action="" method="">
+                        <div class="wizard-navigation">
+                            <div class="progress-with-circle">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="1" aria-valuemin="1" aria-valuemax="3" style="width: 21%;"></div>
                             </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Semester</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Curriculum</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="inputGroupSelect01">Section</label>
-                                    <select class="form-select" id="inputGroupSelect01">
-                                        <option selected>Choose...</option>
-                                        <option value="1">FIRST</option>
-                                        <option value="2">SECOND</option>
-                                        <option value="3">SUMMER</option>
-                                    </select>
-                                </div>
-                            </div>
-
+                            <!-- Progress Nav -->
+                            <ul>
+                                <li>
+                                    <a href="#student_information" data-toggle="tab">
+                                        <div class="icon-circle">
+                                            <i class="bi bi-person-lines-fill" id="bi_resize"></i>
+                                        </div>
+                                        STUDENT INFORMATION
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#advising" data-toggle="tab">
+                                        <div class="icon-circle">
+                                            <i class="bi bi-clipboard-plus" id="bi_resize"></i>
+                                        </div>
+                                        ADVISING
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#registration" data-toggle="tab">
+                                        <div class="icon-circle">
+                                            <i class="bi bi-file-text" id="bi_resize"></i>
+                                        </div>
+                                        REGISTRATION
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#payment" data-toggle="tab">
+                                        <div class="icon-circle">
+                                            <i class="bi bi-cash-stack" id="bi_resize"></i>
+                                        </div>
+                                        PAYMENT
+                                    </a>
+                                </li>
+                            </ul>
+                            <!-- /Progress Nav -->
                         </div>
-                    </div>
-                    <div class="col-md-12">
-                        <hr>
-                        <h6>SUGGESTED SUBJECTS</h6>
-                        <table class="table table-striped" id="table1">
-                            <thead>
-                                <tr>
-                                    <th>Sched Code</th>
-                                    <th>Course Code</th>
-                                    <th>Course Title</th>
-                                    <th>Section</th>
-                                    <th>Units</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>20202021</td>
-                                    <td>ITC123</td>
-                                    <td>Programming Fundamentals</td>
-                                    <td>BSIT1A</td>
-                                    <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
-                                </tr>
-                                <tr>
-                                    <td>20202022</td>
-                                    <td>ITC123</td>
-                                    <td>Programming Fundamentals</td>
-                                    <td>BSIT1A</td>
-                                    <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
-                                </tr>
-                                <tr>
-                                    <td>20202023</td>
-                                    <td>ITC123</td>
-                                    <td>Programming Fundamentals</td>
-                                    <td>BSIT1A</td>
-                                    <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
-                                </tr>
-                                <tr>
-                                    <td>20202023</td>
-                                    <td>ITC123</td>
-                                    <td>Programming Fundamentals</td>
-                                    <td>BSIT1A</td>
-                                    <td>5</td>
-                                    <td><a href="#" class="btn btn-warning rounded-pill">Remove</a></td>
-                                </tr>
+                        <br>
+                        <br>
+                        <br>
+                        <!-- Inside Content -->
+                        <div class="tab-content">
+                            <div class="tab-pane container" id="student_information">
+                                <?php $this->load->view($this->data['student_information']); ?>
+                            </div>
+                            <div class="tab-pane container" id="advising">
+                                <?php $this->load->view($this->data['advising']); ?>
+                            </div>
+                            <div class="tab-pane container" id="registration">
 
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="col-md-12">
-                    </div>
+                            </div>
+                            <div class="tab-pane container" id="payment">
+                                <?php $this->load->view($this->data['payment']); ?>
+                            </div>
+                        </div>
+
+                        <!-- /Inside Content -->
+                        <!-- For Button Next 
+                            <div class="wizard-footer">
+                                <div class="pull-right">
+                                    <input type='button' class='btn btn-next btn-fill btn-warning btn-wd' name='next' value='Next' />
+                                    <input type='button' class='btn btn-finish btn-fill btn-warning btn-wd' name='finish' value='Finish' />
+                                </div>
+
+                                <div class="pull-left">
+                                    <input type='button' class='btn btn-previous btn-default btn-wd' name='previous' value='Previous' />
+                                </div>
+                                <div class="clearfix"></div>
+                            </div> 
+                        -->
+                    </form>
                 </div>
             </div>
-
+            <!-- /wizard container -->
         </div>
+    </div>
+    <?php $this->load->view('Body/Assessment_Content/Advising_Modals'); ?>
+
+
 </section>
-<script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
-<script>
-    // Simple Datatable
-    let table1 = document.querySelector('#table1');
-    let dataTable = new simpleDatatables.DataTable(table1);
-</script>
+<!--   Core JS Files   -->
+<script src="<?php echo base_url() ?>assets/js/wizard/jquery-2.2.4.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/wizard/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/wizard/jquery.bootstrap.wizard.js" type="text/javascript"></script>
+<!--  Plugin for the Wizard -->
+<script src="<?php echo base_url() ?>assets/js/wizard/paper-bootstrap-wizard.js" type="text/javascript"></script>
--- application/views/Layout/Default/Sidenav.php
+++ application/views/Layout/Default/Sidenav.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="index.html"><img src="assets/images/logo/sdcalogo.png" alt="Logo" srcset=""></a>
+                    <a href="index.html"><img src="<?php echo base_url('assets/images/logo/sdcalogo.png');?>" alt="Logo" srcset=""></a>
                     <hr>
                     <h6 style="text-align: center;">ADMISSIONS PORTAL</h6>
                 </div>
@@ -12,41 +12,46 @@
                 </div>
             </div>
         </div>
+
         <div class="sidebar-menu">
             <ul class="menu">
+            <li class="sidebar-title" style="text-align: center;"><h6>Welcome, Juan!</h6></li>
 
-                <li class="sidebar-title" style="text-align: center;"><h6>Welcome, Juan!</h6></li>
-                
                 <li class="sidebar-item">
                     <a href="index.html" class='sidebar-link'>
                         <i class="bi bi-file-earmark-medical-fill"></i>
                         <span>Enrollment Guide</span>
                     </a>
                 </li>
-
                 <li class="sidebar-item <?php echo $tab_active == 'Self Assessment' ? 'active' : ''; ?>">
-                    <a href="index.html" class='sidebar-link'>
+                    <a href="<?php echo base_url('main/selfassesment')?>" class='sidebar-link'>
                         <i class="bi bi-file-earmark-medical-fill"></i>
-                        <span>Self Assessment</span>
+                        <span>Self Assesment</span>
                     </a>
                 </li>
-
-
                 <li class="sidebar-item">
                     <a href="index.html" class='sidebar-link'>
                         <i class="bi bi-map-fill"></i>
                         <span>Enrollment Tracker</span>
                     </a>
                 </li>
-
-
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-gear-fill"></i>
+                        <span>Admin</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="submenu-item <?php echo $tab_active == 'Password Reset' ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url('main/passwordReset')?>">Reset Password</a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="sidebar-item">
-                    <a href="index.html" class='sidebar-link'>
+                    <a href="logout" class='sidebar-link'>
                         <i class="bi bi-box-arrow-left"></i>
                         <span>Logout</span>
                     </a>
                 </li>
-
             </ul>
         </div>
         <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>

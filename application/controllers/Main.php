272c9e61c3cd02534c392c7232610a7b7e612644
Jhon Norman Fabregas <jfabregas@sdca.edu.ph>
2021-05-05 09:29:17 +0800

    feat:include student number and student name ( Proof of Payment)

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -636,7 +636,8 @@ class Main extends MY_Controller
 	public function uploadProofOfPayment()
 	{
 		$checkRequirement = $this->mainmodel->checkRequirement('proof_of_payment');
-
+		$getStudentAccountInfo = $this->mainmodel->getStudentAccountInfo($this->session->userdata('reference_no'));
+		$this->data['student_number'] = $getStudentAccountInfo['Student_Number'];
 		if (!empty($checkRequirement)) {
 			// $result = $this->gdrive_uploader->getFileId(array('file_name'=>$checkRequirement['file_submitted'],'folder_id'=>$this->session->userdata('gdrive_folder')));
 			$this->data['gdrive_link'] = $checkRequirement['path_id'];
@@ -710,7 +711,8 @@ class Main extends MY_Controller
 					'acc_num' => $this->input->post('account_number'),
 					'acc_holder_name' => $this->input->post('holder_name'),
 					'payment_reference_no' => $this->input->post('reference_number'),
-					'ref_no' => $ref_no
+					'ref_no' => $ref_no,
+					'amount_paid' => $this->input->post('amount_paid')
 				));
 			}
 		} else {

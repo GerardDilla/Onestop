97feb324862261e2d0d8546a323dbd163593acb9
Bryan Sampiano <bryansampi08@gmail.com>
2021-05-24 09:13:25 +0800

    Logout pathing

--- application/views/Layout/Default/Sidenav.php
+++ application/views/Layout/Default/Sidenav.php
@@ -77,7 +77,7 @@
                     </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="<?php echo base_url('logout');?>" class='sidebar-link'>
+                    <a href="<?php echo base_url('main/logout');?>" class='sidebar-link'>
                         <i class="bi bi-box-arrow-left"></i>
                         <span>Logout</span>
                     </a>

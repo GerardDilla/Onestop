f0a75aec5257ca35a0d6847fb198efaef3aa934a
jfabregassdca <jfabregas@sdca.edu.ph>
2021-05-26 22:42:59 +0800

    fix conflicts

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://stdominiccollege.edu.ph/Onestop/';
-$config['base_url'] = 'http://localhost/Onestop/';
+$config['base_url'] = 'http://192.168.0.114/Onestop/';
 /*
 |--------------------------------------------------------------------------
 | Index File
